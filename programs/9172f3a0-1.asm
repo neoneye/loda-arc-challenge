56d2b8bed634a6fbc087d2b12916253ed550c3cd
Simon Strandgaard <neoneye@gmail.com>
2022-12-11 00:23:35 +0100

    Gather the programs from the loda_arc_challenge_repository

---
+++ programs/9172f3a0-1.asm
@@ -0,0 +1,5 @@
+; ARC:9172f3a0
+; Submitted by Simon Strandgaard
+; Program Type: simple
+
+f11 $0,101201 ; resize x*3 y*3
---
+++ programs/c59eb873-1.asm
@@ -0,0 +1,5 @@
+; ARC:c59eb873
+; Submitted by Simon Strandgaard
+; Program Type: simple
+
+f11 $0,101200 ; resize x*2 y*2
