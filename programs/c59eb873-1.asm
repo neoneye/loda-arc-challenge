3f54659cbc9250a6b93f0f44ad35eed62413c7fa
Simon Strandgaard <neoneye@gmail.com>
2022-12-11 10:56:57 +0100

    ImageResizeFunction, replaced enum with width x height parameters

--- programs/c59eb873-1.asm
+++ programs/c59eb873-1.asm
@@ -2,4 +2,12 @@
 ; Submitted by Simon Strandgaard
 ; Program Type: simple
 
-f11 $0,101200 ; resize x*2 y*2
+mov $1,$0
+mov $2,$0
+f11 $1,101000 ; get width
+f11 $2,101001 ; get height
+mul $1,2
+mul $2,2
+; $1 is width
+; $2 is height
+f31 $0,101200 ; resize image
