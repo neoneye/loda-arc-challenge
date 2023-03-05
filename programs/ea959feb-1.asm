cab8462cbc0d13b90d670ed01fef4bc0fe0f5306
Simon Strandgaard <neoneye@gmail.com>
2023-03-05 15:29:35 +0100

    More solutions that use the repair_pattern algorithm.

--- programs/ea959feb-1.asm
+++ programs/ea959feb-1.asm
@@ -2,5 +2,5 @@
 ; Program Type: simple
 
 ; $0 is the image that is to be repaired
-mov $1,0 ; repair color
+mov $1,1 ; repair color
 f21 $0,102151 ; Repair damaged pixels and recreate big repeating patterns such as mosaics.
