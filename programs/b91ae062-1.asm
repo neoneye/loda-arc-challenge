c5b69e97890b657aaba243b0fcb67b0844404fbe
Simon Strandgaard <neoneye@gmail.com>
2023-03-08 23:59:40 +0100

    Asserts to check precondidtions, so that we don't waste time on the expensive image operations

--- programs/b91ae062-1.asm
+++ programs/b91ae062-1.asm
@@ -2,15 +2,19 @@
 ; Submitted by Simon Strandgaard
 ; Program Type: simple
 
-mov $5,$0
-f11 $5,101240 ; number of unique colors
-sub $5,1
-
 mov $1,$0
 f11 $1,101000 ; get width
+mov $2,5
+f20 $1,1033 ; Assert input[0] is less than or equal to input[1].
 
 mov $2,$0
 f11 $2,101001 ; get height
+mov $3,5
+f20 $2,1033 ; Assert input[0] is less than or equal to input[1].
+
+mov $5,$0
+f11 $5,101240 ; number of unique colors
+sub $5,1
 
 mul $1,$5
 mul $2,$5
